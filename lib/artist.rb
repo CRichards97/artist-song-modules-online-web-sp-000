e8f3fe61c68beace1e994cf1c3dbd11057f85751
Clint Richards <richardsclint17@gmail.com>
2021-02-14 04:10:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,10 @@
-require_relative '../config/environment'
+require 'pry'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
 
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
   @@artists = []
-
-  module Memorable
-  end
 
   def initialize
     @@artists << self
@@ -27,13 +19,24 @@ class Artist
     @@artists
   end
 
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
   def add_song(song)
     @songs << song
-    song.artist = self unless song.artist
+    song.artist = self
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
